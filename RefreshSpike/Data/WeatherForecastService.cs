132004278b6a12002e34b6b0e87d4cde24ade6d3
Howard Richards <dev@conficient.com>
2021-03-23 10:03:02 +0000

    Modified template to create updating weather forecast demo

--- RefreshSpike/Data/WeatherForecastService.cs
+++ RefreshSpike/Data/WeatherForecastService.cs
@@ -1,25 +1,84 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace RefreshSpike.Data
 {
-    public class WeatherForecastService
+    /// <summary>
+    /// Amended weather forecast service
+    /// </summary>
+    public class WeatherForecastService : IDisposable
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+        private readonly Random rng;
+        private readonly WeatherForecast[] forecasts;
+        private readonly Timer timer;
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        const int INTERVAL_MS = 5000; // every 5 seconds
+
+        /// <summary>
+        /// Generate random data in the ctor
+        /// </summary>
+        public WeatherForecastService()
         {
-            var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var startDate = DateTime.Today;
+            rng = new Random();
+            forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
+            }).ToArray();
+            timer = new Timer(INTERVAL_MS);
+            timer.Elapsed += TimerTick;
+            timer.Enabled = true;
+
         }
+
+
+        /// <summary>
+        /// Get forecasts for inital load
+        /// </summary>
+        /// <returns></returns>
+        public Task<WeatherForecast[]> GetForecastAsync()
+        {
+            return Task.FromResult(forecasts);
+        }
+
+        /// <summary>
+        /// Event raised when a weather forecast changes
+        /// </summary>
+        public event Action<WeatherForecast> OnForecastChanged;
+
+        /// <summary>
+        /// Simulate a data change event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerTick(object sender, ElapsedEventArgs e)
+        {
+            // get random weather forecast entry
+            var index = rng.Next(0, forecasts.Length - 1);
+            var forecast = forecasts[index];
+            // change the entry randomly
+            forecast.TemperatureC = rng.Next(-20, 55);
+            forecast.Summary = Summaries[rng.Next(Summaries.Length)];
+
+            // broadcast the event
+            OnForecastChanged?.Invoke(forecast);
+        }
+
+
+        public void Dispose()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+
     }
 }
